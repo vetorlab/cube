9e5587b841f22ddddb649373c0fc3f2d3f6e6bb6
Felds Liscia <dev@felds.com.br>
2018-02-14 21:39:27 -0200

    move face styles to js

--- src/Face.js
+++ src/Face.js
@@ -1,3 +1,15 @@
+// @ts-check
+
+const transforms = {
+    front: 'rotateY(90deg) translateX(calc(50% - 1px)) rotateY(-90deg)',
+    left: 'translateX(calc(-50% + 1px)) rotateY(90deg)',
+    right: 'translateX(calc(50% - 1px)) rotateY(-90deg)',
+    top: 'translateY(calc(-50% + 1px)) rotateX(-90deg)',
+    bottom: 'translateY(calc(50% - 1px)) rotateX(90deg)',
+    back: 'rotateY(90deg) translateX(calc(-50% + 1px)) rotateY(90deg)',
+}
+
+
 class Face extends HTMLElement {
     static get observedAttributes() {
         return ['src', 'face']
@@ -8,10 +20,10 @@ class Face extends HTMLElement {
     }
 
     set face(face) {
-        const allowedFaces = ['top', 'bottom', 'left', 'right', 'front', 'back']
+        if (!transforms.hasOwnProperty(face))
+            throw `Please use one of the allowed face types: ${Object.keys(transforms).join(', ')}`
         
-        if (!allowedFaces.includes(face))
-            throw `Please use one of the allowed face types: ${allowedFaces.join(', ')}`
+        this.style.transform = transforms[face]
     }
     get face() {
         return this.getAttribute('face')
