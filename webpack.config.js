55eb8871702b6f2fcf54fc1306c3eb5c8c5f2162
Felds Liscia <dev@felds.com.br>
2018-02-14 23:23:18 -0200

    install and configure webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,20 @@
 var path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/main.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
-            {
-                test:       /\.js$/,
-                exclude:    /(node_modules|bower_components)/,
-                use:        { loader: 'babel-loader', options: { presets: [ 'env' ] } },
-            },
-            {
-                test:       /\.css$/,
-                use:        [ 'style-loader', 'css-loader' ],
-            },
+            {test: /\.js$/, exclude: [/node_modules/], use: ['babel-loader']},
+            {test: /\.css$/, use: ['style-loader', 'css-loader']},
         ]
+    },
+    devServer: {
+        contentBase: './',
+        publicPath: "/dist/",
+        open: true,
     }
-};
+}
