f3a1a6d7caa78593ac548353bbc58cb5be10a796
Felds Liscia <dev@felds.com.br>
2018-11-26 14:59:56 -0200

    extract functions

--- src/index.js
+++ src/index.js
@@ -14,33 +14,55 @@ export default class VZCube {
         this.el = el;
         this.images = images;
 
-        el.innerHTML = this.constructor.containerTemplate;
+        // add the basic template to the element
+        el.innerHTML = containerTemplate;
 
-        this._populateFaces();
-    }
+        // store reference to pivot el
+        this._pivot = this.el.querySelector('.vz-cube__pivot');
+        if (!this._pivot) throw new Error(`Couldn't find pivot element.`);
 
-    /**
-     * @private
-     */
-    _populateFaces() {
-        const pivot = this.el.querySelector('.vz-cube__pivot');
-        if (!pivot) throw new Error(`Couldn't find pivot element.`);
+        // create the face objects and add them to the pivot
+        populateFaces(this._pivot, images);
 
-        pivot.innerHTML = Object.keys(this.images)
-            .map(side => `
-                <div
-                    data-face="${side}"
-                    class="vz-cube__face"
-                    style="background-image: ${this.images[side]}"
-                ></div>
-            `)
-            .join("\n");
+        // animation control
+        this.isAnimating = false;
+        this.isDragging = false;
+        this.isFrozen = false;
+
+        // this._draggingMultiplier = 0.1
+
+        // this.yaw = 0
+        // this.pitch = 0
+        // this.roll = 0
+
+        // this._handleMouseDown = this._handleMouseDown.bind(this)
+        // this._handleMouseMove = this._handleMouseMove.bind(this)
+        // this._handleMouseUp = this._handleMouseUp.bind(this)
+        // this._handleTouchStart = this._handleTouchStart.bind(this)
+        // this._handleTouchMove = this._handleTouchMove.bind(this)
+        // this._handleTouchEnd = this._handleTouchEnd.bind(this)
+        // this._processAnimation = this._processAnimation.bind(this)
+        // this._refresh = this._refresh.bind(this)
     }
 }
 
-VZCube.containerTemplate = `
+
+const containerTemplate = `
     <div class="vz-cube">
         <div class="vz-cube__pivot">
         </div>
     </div>
 `;
+
+
+/**
+ * Create the face elements and add them to the el.
+ * 
+ * @param {HTMLElement} el  The pivot element.
+ * @param {ImageSet} images 
+ */
+function populateFaces(el, images) {
+    el.innerHTML = Object.keys(images)
+        .map(side => `<div data-face="${side}" class="vz-cube__face" style="background-image: ${images[side]}" ></div>`)
+        .join("\n");
+}
